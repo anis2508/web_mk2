c45a028dd84cea8ba84e4076c72acdea9d118039
SharifahIzzati68 <18ddt21f1035@student.ptss.edu.my>
2023-09-05 16:12:22 +0800

    Sharifah Nur Izzati

---
+++ mk_2/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+</head>
+<body>
+    <form action="papar.php" method="POST">
+        <label for="nama">Cerita Pengalaman anda:</label><br>
+        <textarea id="pengalaman" name="nama"></textarea>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
